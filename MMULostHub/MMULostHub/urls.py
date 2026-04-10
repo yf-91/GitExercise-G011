2aed47b9c40f64ea606d1716c4f23103dd9247b8
KHO YEE THENG <kho.yee.theng@student.mmu.edu.my>
2026-04-10 13:48:19 +0800

    Create path for admin logina and register

--- MMULostHub/MMULostHub/urls.py
+++ MMULostHub/MMULostHub/urls.py
@@ -19,5 +19,7 @@ from user import views
 
 urlpatterns = [
     path('', views.beginning, name='beginning'),
-    path('login/', views.user_login, name='user-login'),
+    path('user-login/', views.user_login, name='user-login'),
+    path('admin-login/', views.admin_login, name='admin-login'),
+    path('register/', views.register, name='register'),
 ]
--- MMULostHub/user/views.py
+++ MMULostHub/user/views.py
@@ -5,3 +5,9 @@ def beginning(request):
 
 def user_login(request):
     return render(request, 'user/user-login.html')
+
+def admin_login(request):
+    return render(request, 'user/admin-login.html')
+
+def register(request):
+    return render(request, 'user/register.html')
