2e69cdf5b6cc28890015219d0b37ed75d2a207c5
TzeYing007 <tzeying0802@gmail.com>
2026-04-13 02:00:39 +0800

    fixed link html and django

--- MMULostHub/report/urls.py
+++ MMULostHub/report/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('feedback/', view.feedback_view, name='feedback'),
+    path('feedback/', views.feedback_view, name='feedback'),
 ]
