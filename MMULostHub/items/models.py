a422dd10997f01c47bbe84f032f3e46bca79dea4
yf-91 <tan.fee2007@gmail.com>
2026-04-13 17:59:37 +0800

    Completed item categories storage setting

--- MMULostHub/items/models.py
+++ MMULostHub/items/models.py
@@ -4,18 +4,39 @@ from django.db import models
 
 class ItemCategory (models.Model):
     
-    CATEGIRY_CHOICES = [
-        ('electronics', 'Electronic Devices'),
-        ('stationary', 'Stationaries'),
-        ('wallet', 'Wallets'),
-        ('card', 'Cards'),
-        ('bottle', 'Water Bottles'),
-        ('bag', 'Bags'),
-        ('key', 'Keys'),
-        ('umbrella', 'Umbrellas'),
-        ('keychain', 'Keychains'),
-        ('clothes', 'Clothes'),
-        ('shoes', 'Shoes'),
-        ('accessory', 'Accessories'),
-        ('other', 'Other Items'),
+    # ('storage name inside sqlite', 'display name on website')
+    CATEGORY_CHOICES = [
+        (   'electronics'  ,    'Electronic Devices'),
+        (   'stationary'   ,    'Stationaries'      ),
+        (   'wallet'       ,    'Wallets'           ),
+        (   'card'         ,    'Cards'             ),
+        (   'bottle'       ,    'Water Bottles'     ),
+        (   'bag'          ,    'Bags'              ),
+        (   'key'          ,    'Keys'              ),
+        (   'umbrella'     ,    'Umbrellas'         ),
+        (   'keychain'     ,    'Keychains'         ),
+        (   'clothes'      ,    'Clothes'           ),
+        (   'shoes'        ,    'Shoes'             ),
+        (   'accessory'    ,    'Accessories'       ),
+        (   'other'        ,    'Other Items'       ),
     ]
+
+    name = models.CharField
+    (
+        max_length = 100,
+    )
+
+    description = models.TextField
+    (
+        max_length = 10000,
+        blank=True,
+    )
+
+    category = models.CharField
+    (
+        max_length = 100,
+        choices = CATEGORY_CHOICES,
+    )
+
+    def __str__(self):
+        return self.name
