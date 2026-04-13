48c4df6afc0cff6b2636d9c7a7cc6a31c98478e0
yf-91 <tan.fee2007@gmail.com>
2026-04-13 17:26:33 +0800

    items categories added

--- MMULostHub/items/models.py
+++ MMULostHub/items/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# yf-storage (Items Categories)
+
+class ItemCategory (models.Model):
+    
+    CATEGIRY_CHOICES = [
+        ('electronics', 'Electronic Devices'),
+        ('stationary', 'Stationaries'),
+        ('wallet', 'Wallets'),
+        ('card', 'Cards'),
+        ('bottle', 'Water Bottles'),
+        ('bag', 'Bags'),
+        ('key', 'Keys'),
+        ('umbrella', 'Umbrellas'),
+        ('keychain', 'Keychains'),
+        ('clothes', 'Clothes'),
+        ('shoes', 'Shoes'),
+        ('accessory', 'Accessories'),
+        ('other', 'Other Items'),
+    ]
