77507a2855a73ac23b8c88d5d52c9fdf3c86e63a
TzeYing007 <tzeying0802@gmail.com>
2026-04-16 02:56:04 +0800

    fix typo modal

--- MMULostHub/report/admin.py
+++ MMULostHub/report/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Feedback
 
 @admin.register(Feedback)
-class FeedbackAdmin(admin.ModalAdmin):
+class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('id', 'comments', 'created_at')
