6f28303ed31a515a071af67e2027c7dde04504e7
TzeYing007 <tzeying0802@gmail.com>
2026-04-16 01:31:35 +0800

    add comment

--- MMULostHub/report/services.py
+++ MMULostHub/report/services.py
@@ -1,6 +1,8 @@
 from .models import Feedback
 
 def create_feedback(comments, image=None):
+
+    #a service function specifically responsible for saving feedback data to the database
     feedback = Feedback(
         comments=comments,
         image=image
