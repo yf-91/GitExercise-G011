d5ed5b192794293e5379bde7feaeb65bf36e38ef
yf-91 <tan.fee2007@gmail.com>
2026-04-16 02:26:36 +0800

    done some adjustments

--- MMULostHub/items/models.py
+++ MMULostHub/items/models.py
@@ -2,42 +2,24 @@ from django.db import models
 
 # yf-storage (Items Categories)
 
-class ItemCategory (models.Model):
-    
-    # ('storage name inside sqlite', 'display name on website')
-    CATEGORY_CHOICES = [
-        (   'electronics'  ,    'Electronic Devices'),
-        (   'stationary'   ,    'Stationaries'      ),
-        (   'wallet'       ,    'Wallets'           ),
-        (   'card'         ,    'Cards'             ),
-        (   'bottle'       ,    'Water Bottles'     ),
-        (   'bag'          ,    'Bags'              ),
-        (   'key'          ,    'Keys'              ),
-        (   'umbrella'     ,    'Umbrellas'         ),
-        (   'keychain'     ,    'Keychains'         ),
-        (   'clothes'      ,    'Clothes'           ),
-        (   'shoes'        ,    'Shoes'             ),
-        (   'accessory'    ,    'Accessories'       ),
-        (   'other'        ,    'Other Items'       ),
-    ]
+# ('storage name inside sqlite', 'display name on website')
+CATEGORY_CHOICES = [
+    (   'electronics'  ,    'Electronic Devices'),
+    (   'stationary'   ,    'Stationaries'      ),
+    (   'wallet'       ,    'Wallets'           ),
+    (   'card'         ,    'Cards'             ),
+    (   'bottle'       ,    'Water Bottles'     ),
+    (   'bag'          ,    'Bags'              ),
+    (   'key'          ,    'Keys'              ),
+    (   'umbrella'     ,    'Umbrellas'         ),
+    (   'keychain'     ,    'Keychains'         ),
+    (   'clothes'      ,    'Clothes'           ),
+    (   'shoes'        ,    'Shoes'             ),
+    (   'accessory'    ,    'Accessories'       ),
+    (   'other'        ,    'Other Items'       ),
+]
 
-    # Add name for the storage item inside the category
-    # Contoh : Iphone, Nike Shoe ...
-    itemName = models.CharField(
-        max_length = 100,                 # Name can have 100 characters
-    )
-
-    itemCategory = models.CharField(
-        max_length = 100,
-        choices = CATEGORY_CHOICES,
-    )
-
-    # Display name of the item on the admin page
-    # Contoh : √ Laptop ✗ Item Obeject 1
-    def __str__(self):
-        return self.itemName
-
-
+# MMU Location Model
 class MMULocation (models.Model):
     
     location_code = models.CharField(                   # system or url(weblink) will see
@@ -59,53 +41,33 @@ class MMULocation (models.Model):
         blank = True,
     )
 
+    # Display name of the item on the admin page
+    # Contoh : √ Laptop ✗ Item Obeject 1
     def __str__(self):
         return self.location_name
-    
-class LostPost (models.Model):
-    
-    lost_title = models.CharField(
-        max_length = 100,
-    )
 
-    lost_datetime = models.DateTimeField()
 
-    lost_category = models.CharField(
+# Lost and Found Post Model  
+class Post (models.Model):
+    
+    post_type = models.CharField(choices=[('lost','Lost'),('found','Found')])
+
+    post_datetime = models.DateTimeField()
+
+    # Dropdown menu to choose category
+    post_itemcategory = models.CharField(
         max_length = 100,
         choices = ItemCategory.CATEGORY_CHOICES,
     )
 
-    lost_location = models.ForeignKey(
+    post_location = models.ForeignKey(
         MMULocation,
         on_delete = models.CASCADE,
-        null = True,                            # database can be empty
+        null = True,                            # database can be empty / can set in view.py to diffrentiate between lost and found post
         blank = True,                           # form can be empty
     )
 
-    lost_description = models.TextField()
+    post_description = models.TextField()
 
     def __str__(self):
-        return self.lost_title
-
-class FoundPost (models.Model):
-    
-    found_title = models.CharField(
-        max_length = 100,
-    )
-
-    found_datetime = models.DateTimeField()
-
-    found_category = models.CharField(
-        max_length = 100,
-        choices = ItemCategory.CATEGORY_CHOICES,
-    )
-
-    found_location = models.ForeignKey(
-        MMULocation,
-        on_delete = models.CASCADE,
-    )
-
-    found_description = models.TextField()
-
-    def __str__(self):
-        return self.found_title
+        return self.post_title
