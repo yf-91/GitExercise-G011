1aa2e9ee0c6a94a4beafbca07203091c79cd48c2
TzeYing007 <tzeying0802@gmail.com>
2026-04-16 01:47:25 +0800

    connect everything in view.py

--- MMULostHub/report/views.py
+++ MMULostHub/report/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from  .services import create_feedback
 
 def feedback_view(request):
+    if request.method == "POST":
+        comments = request.POST.get('comments')
+        image = request.FILES.get('image-upload') 
+
+        #call service function
+        create_feedback(comments=comments, image=image)
+
+        return render(request, 'report/feedback.html', {'success': True})
+    
     return render(request, 'report/feedback.html')
