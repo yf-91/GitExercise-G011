e8c7c33444f1e46530e8367297615088bb1225da
KHO YEE THENG <kho.yee.theng@student.mmu.edu.my>
2026-04-17 01:34:29 +0800

    Added direct link page

--- MMULostHub/MMULostHub/settings.py
+++ MMULostHub/MMULostHub/settings.py
@@ -116,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_URL = '/user/user-login/'
+LOGIN_REDIRECT_URL = '/user/profile/'
+LOGOUT_REDIRECT_URL = '/user/user-login/'
