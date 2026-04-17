15e3c85e8679c2846d2e92f8d3dcc775150c0920
KHO YEE THENG <kho.yee.theng@student.mmu.edu.my>
2026-04-17 15:26:45 +0800

    Merge successfully with profile

--- MMULostHub/user/urls.py
+++ MMULostHub/user/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin-login/', views.admin_login, name='admin-login'),
     path('register/', views.register, name='register'),
     path('check-email/', views.check_email, name='check_email'),
-    path('profile/', views.profile_view, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('update-bio/', views.update_bio, name='update_bio'),
     path('logout/', views.user_logout, name='logout'),
 ]
