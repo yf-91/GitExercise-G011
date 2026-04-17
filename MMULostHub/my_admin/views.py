31946fdc2e5f2dad2c0f4a536c107740fa0071f4
TzeYing007 <tzeying0802@gmail.com>
2026-04-17 23:47:09 +0800

    save

--- MMULostHub/my_admin/views.py
+++ MMULostHub/my_admin/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def admin_feedback_view(request):
-    return render(request, 'admin/adminfeedback.html')
+    return render(request, 'my_admin/adminfeedback.html')
