25540583a6b6d3c5da094f1c1db9501bc072febb
yf-91 <tan.fee2007@gmail.com>
2026-04-17 22:42:40 +0800

    done coding simple create post features

--- MMULostHub/items/models.py
+++ MMULostHub/items/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# yf-storage (Items Categories)
 
 # ('storage name inside sqlite', 'display name on website')
 CATEGORY_CHOICES = [
@@ -44,11 +44,16 @@ class MMULocation (models.Model):
     # Display name of the item on the admin page
     # Contoh : √ Laptop ✗ Item Obeject 1
     def __str__(self):
-        return self.location_name
+        return self.location_code
 
 
 # Lost and Found Post Model  
 class Post (models.Model):
+
+    post_user = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,                             # if user deleted , all related post or data also will be deleted
+    )
     
     post_type = models.CharField(choices=[('lost','Lost'),('found','Found')])
 
@@ -57,7 +62,7 @@ class Post (models.Model):
     # Dropdown menu to choose category
     post_itemcategory = models.CharField(
         max_length = 100,
-        choices = ItemCategory.CATEGORY_CHOICES,
+        choices = CATEGORY_CHOICES,
     )
 
     post_location = models.ForeignKey(
