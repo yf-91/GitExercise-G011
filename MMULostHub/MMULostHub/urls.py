bbf0c7672843a8e5a61bf53651d46309b94c7eec
TzeYing007 <tzeying0802@gmail.com>
2026-04-17 23:40:50 +0800

    fix bug

--- MMULostHub/MMULostHub/urls.py
+++ MMULostHub/MMULostHub/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from admin import views
+from my_admin import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
